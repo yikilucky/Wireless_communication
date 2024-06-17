5fab2302af26c56e552485009bd50a69b69971ee
yikilucky <yikifeng101@gmail.com>
2024-06-17 08:26:04 +0800

    first version

---
+++ src/clusters.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def create_clusters():
+    # Simulation window parameters
+    x_min = -5
+    x_max = 5
+    y_min = -5
+    y_max = 5
+
+    # Parameters for the parent and daughter point processes
+    # lambda_parent = 0.3  # density of parent Poisson point process
+    lambda_daughter = 100  # mean number of points in each cluster
+    radius_cluster = 0.5  # radius of cluster disk (for daughter points)
+
+    # rectangle dimensions
+    x_delta = x_max - x_min
+    y_delta = y_max - y_min
+    area_total = x_delta * y_delta  # area of  rectangle
+
+    # Simulate Poisson point process for the parents
+    # num_parent_points = np.random.poisson(area_total * lambda_parent)  # Poisson number of points
+    num_parent_points = 9
+    # x and y coordinates of Poisson points for the parent
+    xx_parent = x_min + x_delta * np.random.uniform(0, 1, num_parent_points)
+    yy_parent = y_min + y_delta * np.random.uniform(0, 1, num_parent_points)
+
+    xx_parent = np.append(xx_parent, [0])
+    yy_parent = np.append(yy_parent, [0])
+    num_parent_points += 1
+
+    # Simulate Poisson point process for the daughters (ie final poiint process)
+    num_daughter_points = np.random.poisson(lambda_daughter, num_parent_points-1)
+    num_daughter_points = np.append(num_daughter_points, [100])
+    sum_daughters = sum(num_daughter_points)  # total number of points
+
+
+    # Generate the (relative) locations in polar coordinates by
+    # simulating independent variables.
+    theta = 2 * np.pi * np.random.uniform(0, 1, sum_daughters)  # angular coordinates
+    rho = radius_cluster * np.sqrt(np.random.uniform(0, 1, sum_daughters))  # radial coordinates
+
+    # Convert from polar to Cartesian coordinates
+    xx0 = rho * np.cos(theta)
+    yy0 = rho * np.sin(theta)
+
+    # replicate parent points (ie centres of disks/clusters)
+    xx = np.repeat(xx_parent, num_daughter_points)
+    yy = np.repeat(yy_parent, num_daughter_points)
+
+    # translate points (ie parents points are the centres of cluster disks)
+    xx = xx + xx0  # one-dimension
+    yy = yy + yy0  # one-dimension
+
+    xx_new = np.split(xx, np.cumsum(num_daughter_points)[:-1])
+    yy_new = np.split(yy, np.cumsum(num_daughter_points)[:-1])
+
+    xx_new = np.array(xx_new, dtype="object")  # two-dimension
+    yy_new = np.array(yy_new, dtype="object")  # two-dimension
+
+    # thin points if outside the simulation window
+
+    def check_array(array1, array2, lower, upper):
+        result = []
+        for ii in range(len(array1)):
+            row_result = []
+            for jj in range(len(array1[ii])):
+                if array1[ii][jj] <= lower or array1[ii][jj] >= upper or array2[ii][jj] <= lower or array2[ii][jj] >= upper:
+                    row_result.append(False)
+                else:
+                    row_result.append(True)
+            result.append(row_result)
+        return result
+
+    boole_results = check_array(xx_new, yy_new, x_min, x_max)  # two-dimension
+
+    devices_x = {}
+    for i in range(len(xx_new)):
+        devices_x[i] = xx_new[i][np.array(boole_results[i])]
+
+    devices_y = {}
+    for i in range(len(yy_new)):
+        devices_y[i] = yy_new[i][np.array(boole_results[i])]
+
+    return num_parent_points, xx_parent, yy_parent, devices_x, devices_y
---
+++ src/interference.py
@@ -0,0 +1,40 @@
+from collections import OrderedDict
+import numpy as np
+from scipy import integrate
+import cmath
+from tqdm.auto import tqdm
+import torch
+
+
+def generate_interference(model_parameters, num_servers, servers_xx, servers_yy, devices_xx, devices_yy):
+    interference = OrderedDict()
+
+    def f(x):
+        return (1 / x) * np.exp(-x)
+
+    Ei, err = integrate.quad(f, 0.2, np.inf)
+
+    rho = (6*0.25) / (2 * Ei * 0.016)
+
+    for key in model_parameters.keys():
+        mean = torch.mean(model_parameters[key]).item()
+        var = torch.var(model_parameters[key]).item()
+        for cluster_idx in range(num_servers-1):
+            num_selected_clients = 10
+            selected_client_indices = sorted(np.random.choice(a=[i for i in range(len(devices_xx[cluster_idx]))], size=num_selected_clients, replace=False).tolist())
+            for device_idx in tqdm(selected_client_indices, leave=False):
+                h1_squared = np.random.exponential(1, 1)[0]
+                if h1_squared < 0.2:
+                    continue
+                r1 = np.sqrt((devices_xx[cluster_idx][device_idx]-servers_xx[cluster_idx]) ** 2 + (devices_yy[cluster_idx][device_idx]-servers_yy[cluster_idx]) ** 2)
+                power = np.sqrt(rho) * (r1 ** 1.5) / cmath.rect(np.sqrt(h1_squared), np.random.uniform(0, 2 * cmath.pi))
+                h2 = cmath.rect(np.sqrt(np.random.exponential(1, 1)[0]), np.random.uniform(0, 2 * cmath.pi))
+                r2 = np.sqrt((devices_xx[cluster_idx][device_idx]) ** 2 + (devices_yy[cluster_idx][device_idx]) ** 2)
+                coefficient = (power * h2) / (r2 ** 1.5)
+                if key not in interference.keys():
+                    interference[key] = coefficient.real * torch.tensor(np.random.normal(mean, var, tuple(model_parameters[key].shape)), dtype=torch.float32)
+                else:
+                    interference[key] += coefficient.real * torch.tensor(np.random.normal(mean, var, tuple(model_parameters[key].shape)), dtype=torch.float32)
+
+    return interference, rho
+
